479ce370cb1e71cece413bf743385d55225f70c9
codedbychavez <codedbychavez@gmail.com>
2022-08-19 14:35:35 -0300

    Init

---
+++ src/components/WriterPad.vue
@@ -0,0 +1,50 @@
+<script>
+import { useTextSelection } from '@vueuse/core'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+export default {
+    setup() {
+      const state = useTextSelection();
+      return {
+        state,
+      }
+    },
+    data() {
+      return {
+        editor: ClassicEditor,
+        editorData: '<p>Content of the editor.</p>',
+
+      }
+    },
+    computed: {
+      selectedText() {
+        return this.state.text.value
+      }
+    },
+    methods: {
+      applyTag(tag) {
+        switch (tag) {
+          case 'bold':
+            
+            break;
+        
+          default:
+            break;
+        }
+      }
+    }
+}
+</script>
+
+<template>
+  <div class="p-4">
+    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+  </div>
+</template>
+
+
+<style>
+.ck-editor__editable {
+    min-height: 500px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import CKEditor from '@ckeditor/ckeditor5-vue';
 
-import './assets/main.css'
+import './index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(CKEditor)
 
 app.mount('#app')
